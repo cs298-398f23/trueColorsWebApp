da97ed5f8b75ecaccae69ad2bc5bd5a3dd2c3ff6
MichaelRomero1 <143423762+MichaelRomero1@users.noreply.github.com>
2023-11-14 17:26:14 -0500

    Update app.js
    Explained why the javascript file is currently empty, as I put the javascript code in a script tag in the html

--- static/app.js
+++ static/app.js
@@ -1,1 +1,2 @@
-
+//The code that I had for JavaScript, I couldn't get the server to find it for some reason. I moved it inside the html as a script tag, and that worked
+//Just thought I should comment here to avoid confusion
