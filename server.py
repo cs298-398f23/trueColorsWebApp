f9f8a0f8c7eedce983fe704ae2dcf82bdfef255d
MichaelRomero1 <143423762+MichaelRomero1@users.noreply.github.com>
2023-11-14 17:30:40 -0500

    Update server.py
    Imported render_template in order to render the html upon server startup. There is also a function saveScore I made to try and get something to happen when I clicked the begin button. Comes as a GET as of now however, but it does display the message. Kept Raf's code with the long endpoint just in case.

--- server.py
+++ server.py
@@ -1,14 +1,31 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
-# TEMPORARY ROUTE for receiving each score from the assessment.
-@app.route('/saveScore/<score1>/<score2>/<score3>/<score4>', methods=['POST'])
-def saveScore(score1, score2, score3, score4):
-    return jsonify({'score1': score1,
-                    'score2': score2,
-                    'score3': score3,
-                    'score4': score4})
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return render_template("index.html")
+
+
+    # TEMPORARY ROUTE for receiving each score from the assessment.
+    @app.route("/quiz.php", methods=['GET', 'POST'])
+    def saveScore():
+        return "score saved"
+
+
+    # TEMPORARY ROUTE for receiving each score from the assessment.
+    @app.route('/saveScore/<score1>/<score2>/<score3>/<score4>', methods=['POST'])
+    def saveScore(score1, score2, score3, score4):
+        return jsonify({'score1': score1,
+                        'score2': score2,
+                        'score3': score3,
+                        'score4': score4})
+    return app
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app = create_app()
+    app.run(debug=True, port=8000, host="0.0.0.0") #Using the debugging development server for now but we can switch to GUnicorn later
+
