9d7ad59b757be244a34532e463bfb3b063c023a9
Rafael Garcia Jr <143424787+RGJ-713@users.noreply.github.com>
2023-11-20 09:57:49 -0500

    Update server.py
    Reverted to an earlier working state.

--- server.py
+++ server.py
@@ -1,44 +1,17 @@
 from flask import Flask, jsonify, request, render_template
-from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_USER'] = 'project'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'project'
-app.config['MYSQL_DATABASE_DB'] = 'true_colors_assessment'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-
-mysql = MySQL()
-mysql.init_app(app)
-
 def create_app():
-    app = Flask(__name__)
 
     @app.route('/')
     def index():
-        try:
-            cursor = mysql.get_db().cursor()
-        
-            cursor.execute("SELECT * FROM users")
-            cursor.execute("SELECT * FROM answers_group_1")
-            cursor.execute("SELECT * FROM answers_group_2")
-            cursor.execute("SELECT * FROM answers_group_3")
-            cursor.execute("SELECT * FROM answers_group_4")
-            cursor.execute("SELECT * FROM answers_group_5")
-            
-             # Fetch all of the data from the tables
-            data = cursor.fetchall()
-            
-        finally:
-            cursor.close()
         return render_template("index.html", data=data)
-
 
     # TEMPORARY ROUTE for receiving each score from the assessment.
     @app.route("/quiz", methods=['GET', 'POST'])
     def saveScore():
         return "score saved"
-
 
     # TEMPORARY ROUTE for receiving each score from the assessment.
     @app.route('/saveScore/<score1>/<score2>/<score3>/<score4>', methods=['POST'])
