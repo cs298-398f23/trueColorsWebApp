56eafb9770f6454c725be547aed2b1389f8f7693
Rafael Garcia Jr <143424787+RGJ-713@users.noreply.github.com>
2023-12-11 12:00:29 -0500

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -1,14 +1,16 @@
 import pytest
 from server import create_app
 
+# Return a basic user ID for testing
 def getId():
-    # Define the logic to get the ID
     return '1234'
 
+# Test the app is created with the correct secret key
 def test_create_app():
     app = create_app()
     assert app.secret_key == 'secret'
-    
+
+# Test the session UUID is created and stored correctly
 def test_getId():
     app = create_app()
     with app.test_client() as test:
@@ -16,6 +18,16 @@ def test_getId():
             sess['uuid'] = '1234'
         with app.app_context():
             assert getId() == '1234'
-            
+
+# Test the questions are returned correctly
 def test_get_questions():
     app = create_app()
+    with app.test_client() as test:
+        with test.session_transaction() as sess:
+            sess['uuid'] = '1234'
+        with app.app_context():
+            assert test.get('/getQuestions?group=1').status_code == 200
+            assert test.get('/getQuestions?group=2').status_code == 200
+            assert test.get('/getQuestions?group=3').status_code == 200
+            assert test.get('/getQuestions?group=4').status_code == 200
+            assert test.get('/getQuestions?group=5').status_code == 200
