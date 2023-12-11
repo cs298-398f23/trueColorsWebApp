f064d6c12448de6a5ed8c36d2979eafc99d7f027
Rafael Garcia Jr <143424787+RGJ-713@users.noreply.github.com>
2023-12-11 12:09:55 -0500

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -31,3 +31,20 @@ def test_get_questions():
             assert test.get('/getQuestions?group=3').status_code == 200
             assert test.get('/getQuestions?group=4').status_code == 200
             assert test.get('/getQuestions?group=5').status_code == 200
+
+# Test the correct status codes for storing a result
+def test_store_result_correct_status_codes():
+    app = create_app()
+    with app.test_client() as test:
+        with test.session_transaction() as sess:
+            sess['uuid'] = '1234'
+        with app.app_context():
+            assert test.post('/storeResult/ORANGE').status_code == 200
+            assert test.post('/storeResult/GOLD').status_code == 200
+            assert test.post('/storeResult/BLUE').status_code == 200
+            assert test.post('/storeResult/GREEN').status_code == 200
+
+            assert test.post('/storeResult/12').status_code == 400
+            assert test.post('/storeResult/QR5').status_code == 400
+            assert test.post('/storeResult/RED').status_code == 400
+            assert test.post('/storeResult/PURPLE').status_code == 400
