7340e416af5b4ccbbb65894da6c9350fc29fef73
MichaelRomero1 <143423762+MichaelRomero1@users.noreply.github.com>
2023-12-10 19:49:14 -0500

    Update app.js
    Added a bunch of code to utilize local storage in order to save user's current progress on the test upon refresh

--- static/app.js
+++ static/app.js
@@ -1,4 +1,5 @@
-// Get main test panels and hide them
+// Get msin test panels and hide them
+
 const start_panel = document.getElementById('start_panel');
 const test_container = document.getElementById('test_container');
 const results_panel = document.getElementById('results_panel');
@@ -36,9 +37,14 @@ let answers_3 = [];
 let answers_4 = [];
 let answers_5 = [];
 
-// Define group variable and load first set of questions
-let group = 0;
-loadQuestions();
+// Define group variable to start at 1 if there is currently nothing in local storage
+if (localStorage.getItem('currentGroup') === null) {
+    saveState(1);
+}
+
+console.log(loadState())
+let group = parseInt(loadState());
+console.log('Current group:', group);
 
 // Function that ensures each new question doesn't already have an option selected
 function deselectAnswers() {
@@ -58,8 +64,8 @@ async function fetchQuestionsFromDatabase() {
 // Function that loads the current question information
 async function loadQuestions() {
 
-    deselectAnswers();
-    group++;
+    //deselectAnswers();
+    
     const questions = await fetchQuestionsFromDatabase();
 
     // Load questions into HTML
@@ -85,34 +91,37 @@ async function loadQuestions() {
 
 }
 
-// Event listener to hide start panel and begin test
+// Event listener to start test and hide start panel
 start_button.addEventListener('click', () => {
 
     start_panel.style = 'display: none';
     test_container.style = 'display: contents';
+
+    loadQuestions();
 
 })
 
 // Function to calculate the scores for each of the four colors
 function getScores() {
 
-    // Get score for ORANGE - AHKNS
-    const score_orange = parseInt(answers_1[0]) + parseInt(answers_2[3]) + parseInt(answers_3[2]) + parseInt(answers_4[1])
-                       + parseInt(answers_5[2]);
+    // Get score for Orange - AHKNS
+    const score_orange = parseInt(JSON.parse(localStorage.getItem('answers_1'))[0]) + parseInt(JSON.parse(localStorage.getItem('answers_2'))[3]) + parseInt(JSON.parse(localStorage.getItem('answers_3'))[2]) + parseInt(JSON.parse(localStorage.getItem('answers_4'))[1])
+                       + parseInt(JSON.parse(localStorage.getItem('answers_5'))[2]);
 
-    // Get score for BLUE - CFJOR
-    const score_blue = parseInt(answers_1[2]) + parseInt(answers_2[1]) + parseInt(answers_3[1]) + parseInt(answers_4[2]) 
-                     + parseInt(answers_5[1]);
+    // Get score for Blue - CFJOR
+    const score_blue = parseInt(JSON.parse(localStorage.getItem('answers_1'))[2]) + parseInt(JSON.parse(localStorage.getItem('answers_2'))[1]) + parseInt(JSON.parse(localStorage.getItem('answers_3'))[1]) + parseInt(JSON.parse(localStorage.getItem('answers_4'))[2]) 
+                     + parseInt(JSON.parse(localStorage.getItem('answers_5'))[1]);
 
-    // Get score for GOLD - BGIMT
-    const score_gold = parseInt(answers_1[1]) + parseInt(answers_2[2]) + parseInt(answers_3[0]) + parseInt(answers_4[0]) 
-                     + parseInt(answers_5[3]);
+    // Get score for Gold - BGIMT
+    const score_gold = parseInt(JSON.parse(localStorage.getItem('answers_1'))[1]) + parseInt(JSON.parse(localStorage.getItem('answers_2'))[2]) + parseInt(JSON.parse(localStorage.getItem('answers_3'))[0]) + parseInt(JSON.parse(localStorage.getItem('answers_4'))[0]) 
+                     + parseInt(JSON.parse(localStorage.getItem('answers_5'))[3]);
 
-    // Get score for GREEN - DELPQ
-    const score_green = parseInt(answers_1[3]) + parseInt(answers_2[0]) + parseInt(answers_3[3]) + parseInt(answers_4[3]) 
-                      + parseInt(answers_5[0])
+    // Get score for Green - DELPQ
+    const score_green = parseInt(JSON.parse(localStorage.getItem('answers_1'))[3]) + parseInt(JSON.parse(localStorage.getItem('answers_2'))[0]) + parseInt(JSON.parse(localStorage.getItem('answers_3'))[3]) + parseInt(JSON.parse(localStorage.getItem('answers_4'))[3]) 
+                      + parseInt(JSON.parse(localStorage.getItem('answers_5'))[0])
 
     const scores = [score_orange, score_blue, score_gold, score_green];
+    console.log(scores)
     return scores;
 }
 
@@ -121,15 +130,15 @@ function getHighestScore() {
     const scores = getScores();
     const highest_score = Math.max(...scores);
 
-    if (highest_score == scores[0]) {
+    if (highest_score === scores[0]) {
         return 'ORANGE';
     }
 
-    else if (highest_score == scores[1]) {
+    else if (highest_score === scores[1]) {
         return 'BLUE';
     }
 
-    else if (highest_score == scores[2]) {
+    else if (highest_score === scores[2]) {
         return 'GOLD';
     }
 
@@ -144,21 +153,22 @@ function getAnswers() {
     const questionBlocks = document.querySelectorAll('.question_block');
     let answers = [];
 
-    // For each question block, get the answer that was selected
     questionBlocks.forEach((questionBlock) => {
         const answerEls = questionBlock.querySelectorAll('.answer');
         let answerFound = false;
 
         answerEls.forEach((answerEl) => {
             if (answerEl.checked) {
-                answers.push(answerEl.id); // If answer is found, add it to answers
+                answers.push(answerEl.id);
                 answerFound = true;
             }
         });
-
+        //Handles the case where no answer's found
         if (!answerFound) {
-            answers.push(undefined); // If answer is not found, add undefined to signal a missing answer
+            //Default value
+            answers.push(undefined);
         }
+        
     });
 
     return answers;
@@ -168,67 +178,47 @@ function getAnswers() {
 // Function to check if all answers have are unique and have been selected
 function checkAnswersRecorded(answers) {
 
-    // If no answer selected, return false
     if (answers.includes(undefined)) {
         return false;
     }
 
-    // If duplicate answers, return false
     for (i = 0; i < answers.length; i++) {
         for (j = i + 1; j < answers.length; j++) {
-            if (answers[i] == answers[j]) {
+            if (answers[i] === answers[j]) {
                 return false;
             }
         }
     }
 
-    // Store answers in correct array
-    if (group == 1) {
+    if (group === 1) {
         answers_1 = answers;
+        localStorage.setItem('answers_1', JSON.stringify(answers_1))
     }
     
-    else if (group == 2) {
+    else if (group === 2) {
         answers_2 = answers;
+        localStorage.setItem('answers_2', JSON.stringify(answers_2))
     }
     
-    else if (group == 3) {
+    else if (group === 3) {
         answers_3 = answers;
+        localStorage.setItem('answers_3', JSON.stringify(answers_3))
+
     }
     
-    else if (group == 4) {
+    else if (group === 4) {
         answers_4 = answers;
+        localStorage.setItem('answers_4', JSON.stringify(answers_4))
+
     }
 
     else {
         answers_5 = answers;
+        localStorage.setItem('answers_5', JSON.stringify(answers_5))
+
     }
 
     return true;
-}
-
-// Function to display the results panel and store the user's result in the database
-function displayResults() {
-
-    // Hide test container and display results panel
-    test_container.style = 'display: none';
-    results_panel.style = 'display:';
-
-    // Get user's result and display it
-    const result_color = getHighestScore();
-    const user_result = document.getElementById('user_result');
-
-    user_result.textContent = result_color;
-    user_result.style = `color: ${result_color}`;
-
-    // Store user's result in database
-    fetch(`/storeResult/${result_color}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({result: result_color})
-    });
-
 }
 
 // Submit button to validate answers and update questions
@@ -240,32 +230,38 @@ submit_button.addEventListener('click', async () => {
     // If all answers are valid, update questions
     if (answerStatus) {
 
+        group++;
+        saveState(group);
+
         // If last question, hide test container and display results panel
-        if (group == 5) {
-
+        if (group >= 6) {
             displayResults();
-
         }
 
         // Else, update questions and change buttons as needed
         else {
 
             // Change 'Begin Test' to 'Resume Test' if test is in progress
-            if (group >= 1) {
+            if (group <= 5) {
                 start_button.textContent = 'Resume Test';
             }
 
             // Change 'Next' to 'Submit Test' if last question
-            if (group == 4) {
+            if (group === 5) {
                 submit_button.textContent = 'Submit Test'; 
             }
+        
+        deselectAnswers(); // Make sure none of the buttons are selected on the next quesion
+
+        // Resets the radio buttons local storage to null
+        removeRadioSelectionsFromLS();
+        
+        saveState(group); // Save the current group to local storage
 
         loadQuestions();
 
         }
-
     }
-
     // Else, alert user to select all answers and make sure they are unique
     else {
         alert("Please do not have duplicate answers and make sure to answer all questions before continuing.")
@@ -273,7 +269,7 @@ submit_button.addEventListener('click', async () => {
 
 })
 
-// Back button to hide current test container and display the instructions
+// Back buttons to hide current test container and display the instructions
 back_button.addEventListener('click', () => {
 
     test_container.style = 'display: none';
@@ -283,14 +279,18 @@ back_button.addEventListener('click', () => {
 
 // Retake button to hide results panel, display the start panel, and reset the test
 retake_button.addEventListener('click', () => {
-
-    // Resetting display properties
+    console.log('Retake button clicked');
+    localStorage.clear();
+    saveState(1);
+    // Resetting display property
     results_panel.style = 'display: none';
     start_panel.style = 'display:';
     
     // Reset the group to 0 when retaking the test
-    group = 0;
-    loadQuestions();
+    deselectAnswers();
+    group = parseInt(loadState());
+    
+    loadQuestions(); 
 
     // Reset button text
     start_button.textContent = 'Begin Test';
@@ -309,7 +309,7 @@ info_button.addEventListener('click', () => {
 
 })
 
-// Info back button to hide info panel and display results panel again
+// Back button to hide info panel and display results panel again
 back_button_info.addEventListener('click', () => {
             
     info_panel.style = 'display: none';
@@ -319,19 +319,74 @@ back_button_info.addEventListener('click', () => {
 
 // Function that tells the user about their color
 function getColorDescription(color) {
-    if (color == 'ORANGE') {
+    if (color === 'ORANGE') {
         return '<strong>Your True Color is ORANGE!</strong><br><br>Others often describe you as fun, energetic, and charismatic. People are naturally drawn to you, and you waste no time getting to know new people. You are naturally extraverted and do very well in social situations.<br><br>You are also incredibly charming and quick-witted. You are a great negotiator who has a knack for convincing others to see things your way.<br><br>Orange individuals are also very creative and spontaneous. You are definitely not afraid to take risks. Similarly, you are often guided by your heart, rather than your head, which sometimes gets you into trouble. You sometimes have trouble following through on long-term goals, and prefer short-term, tangible rewards.<br><br>Because of their need for excitement and socialization, orange individuals are well-suited for careers that are flexible and allow them to work with others. These include careers in education, sales and marketing, management, and customer service.';
     }
 
-    else if (color == 'GOLD') {
+    else if (color === 'GOLD') {
         return '<strong>Your True Color is GOLD!</strong><br><br>You are very detail-oriented and love to plan ahead. You are very predictable and responsible, which gives you a sense of security in life. You rarely do anything that is unpredictable or unplanned.<br><br>You also value history and tradition. You hold your family\'s shared beliefs very closely, and have a high moral standard. You find joy in expressing these values in different aspects of your life, whether that be through helping others by volunteering or through your career. You enjoy having others rely on you to help.<br><br>You enjoy being the best at what you do, and believe there is a right way to do everything. Others would describe you as a "rule-follower" who "does things by the book." Because of this, you can also become pushy and judgmental when you don\'t agree with others\' decisions.<br><br>Because of their need for structure, security, and predictability, those whose True Color is gold do well in jobs that are very straightforward and have highly defined duties.  You enjoy knowing what your tasks will be for the day, and would not perform well in careers that are highly unpredictable. Gold individuals are well-suited for careers in finance or public service.';
     }
 
-    else if (color == 'BLUE') {
+    else if (color === 'BLUE') {
         return '<strong>Your True Color is BLUE!</strong><br><br>You are typically calm, optimistic, and kind. You are a genuinely caring and compassionate individual who tries to see the best in others and in every situation. In stressful situations, you are able to remain calm and mediate situations between individuals.<br><br>Your personality gives you a deep desire to feel appreciated and loved by others. In your family life, you are always giving words of affirmation, and expect the same in return. Your romantic relationships are based on a mutual trust and understanding, and you never waste an opportunity to let your significant other know that you love them - whether that be through a kind word or a kiss. You need a partner who does the same, and you feel undervalued in relationships without daily affirmation of your love and commitment.<br><br>Your positive attitude motivates others. You are not a leader who is the loudest or most charismatic, but rather, you are a quiet leader who inspires others through your own hard work and kindness toward others. You truly lead by example.<br><br>Those whose True Color is blue do well in caring occupations that allow them to make a difference in the lives of others. They often are most successful as faith leaders, counselors, medical professionals, or educators.';
     } 
 
-    else if (color == 'GREEN') {
+    else if (color === 'GREEN') {
         return '<strong>Your True Color is GREEN!</strong><br><br>Others often describe you as extremely intelligent. You have an innate desire to do things to the best of your ability and take pride in your work. At work, you set the standard for everyone else. To you, work is truly enjoyable. Even on the weekends, you can\'t stand the thought of wasting a day by doing nothing, and always try to fit as much into each day as possible.<br><br>You are also a big thinker. You enjoy talking with others about abstract, philosophical ideas. You enjoy thinking about the future and all of its possibilities. You believe that you should never stop learning, and enjoy learning new information simply for your own enjoyment. In the workplace, you are a creative "idea person" who comes up with creative and practical solutions to problems.<br><br>You are very independent. Although you enjoy spending time with others, you need your private time each day to recharge. This gives you time to process everything.<br><br>Individuals with green personalities do well in careers that allow them to apply all of the "big ideas" they have. They typically succeed in careers within higher education, science, technology development, and medicine.';
     }
+}
+
+// Function that saves the current group number to local storage
+function saveState(group) {
+    localStorage.setItem('currentGroup', group);
+}
+
+// Function that loads the current group number from local storage
+function loadState() {
+    return localStorage.getItem('currentGroup');
+}
+
+// Actions to be performed when the page is loaded
+document.addEventListener('DOMContentLoaded', function () {
+    radioButtonLocalStorage();
+
+    if (group === 1) {
+        start_button.textContent = 'Begin Test';
+    }
+
+    if (group === 5) {
+        submit_button.textContent = 'Submit Test'; 
+    }
+});
+
+function displayResults() {
+    console.log('Group:', group);
+    test_container.style = 'display: none';
+    results_panel.style = 'display:';
+
+    // Get user's result and display it
+    const result_color = getHighestScore();
+    const user_result = document.getElementById('user_result');
+
+    user_result.textContent = result_color;
+    user_result.style = `color: ${result_color}`;
+
+    // Store user's result in database
+    fetch(`/storeResult/${result_color}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({result: result_color})
+    }); 
+    
+    localStorage.removeItem('currentGroup');
+    removeRadioSelectionsFromLS();
+}
+
+function removeRadioSelectionsFromLS() {
+    localStorage.removeItem('radioSelectionA');
+    localStorage.removeItem('radioSelectionB');
+    localStorage.removeItem('radioSelectionC');
+    localStorage.removeItem('radioSelectionD');
 }
