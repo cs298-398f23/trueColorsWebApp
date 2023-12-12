49d6f78fdc1f4fbd5084d8c5b254a75073555839
MichaelRomero1 <143423762+MichaelRomero1@users.noreply.github.com>
2023-12-12 07:49:31 -0500

    Update server.py
    Testing to see if running it on localhost for host with the maria db will work?

--- server.py
+++ server.py
@@ -100,7 +100,7 @@ def connectToMySQL():
     '''
     Connects to MySQL and returns a cursor and connection object.
     '''
-    cnx = mysql.connector.connect(user='project', password='project',
+    cnx = mysql.connector.connect(user='root', password='password',
                                   host='localhost',
                                   database='test_db')
     cursor = cnx.cursor()
